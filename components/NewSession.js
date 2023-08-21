d4da719acf8db694defc301040bb0f0309f0b931
Manav Malhotra <manavmalhotrafrnd4u@gmail.com>
2023-08-21 20:15:52 +0530

    Create New Session

--- components/NewSession.js
+++ components/NewSession.js
@@ -5,34 +5,66 @@ import {
   StyleSheet,
   SafeAreaView,
   Button,
+  TouchableOpacity,
 } from "react-native";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
-const NewSession = () => {
-    const [title, setTitle] = useState("");
-    const [description, setDescription] = useState("");
-    const [duration, setDuration] = useState("");
-    const [capacity, setCapacity] = useState("");
+import { SocketManager } from "../SocketManager";
+
+const NewSession = ({ navigation }) => {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [duration, setDuration] = useState("");
+  const [capacity, setCapacity] = useState("");
+  const [socket, setSocket] = useState();
+  const [userId, setUserId] = useState("");
+
+  useEffect(() => {
+    const socket = SocketManager.getSocket();
+    setSocket(socket);
+    setUserId(socket.id);
+  }, []);
+  const createRoom = () => {
+    console.log("Creating room...");
+    console.log("socket id: ", userId);
+    const data = { userId, title, description, duration, capacity };
+
+    socket.emit("createRoom", data);
+    navigation.navigate("Home");
+  };
+
   return (
     <SafeAreaView>
       <View>
         <Text>Title</Text>
-        <TextInput style={styles.codeTextBox} onChangeText={(text) => {
-              setTitle(text);
-            }} />
+        <TextInput
+          style={styles.codeTextBox}
+          onChangeText={(text) => {
+            setTitle(text);
+          }}
+        />
         <Text>Description</Text>
-        <TextInput style={styles.codeTextBox} onChangeText={(text) => {
-              setDescription(text);
-            }} />
+        <TextInput
+          style={styles.codeTextBox}
+          onChangeText={(text) => {
+            setDescription(text);
+          }}
+        />
         <Text>Duration</Text>
-        <TextInput style={styles.codeTextBox} onChangeText={(text) => {
-              setDuration(text);
-            }} />
+        <TextInput
+          style={styles.codeTextBox}
+          onChangeText={(text) => {
+            setDuration(text);
+          }}
+        />
         <Text>Capacity</Text>
-        <TextInput style={styles.codeTextBox} onChangeText={(text) => {
-              setCapacity(text);
-            }} />
-        <Button title="Create" />
+        <TextInput
+          style={styles.codeTextBox}
+          onChangeText={(text) => {
+            setCapacity(text);
+          }}
+        />
+        <Button title="Create Room" onPress={createRoom} />
       </View>
     </SafeAreaView>
   );
